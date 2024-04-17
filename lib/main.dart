2a9ba629e77f7958ecdfcb29431a081a33f5dfc2
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2024-04-17 15:23:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: TextButton(onPressed: (){ },child: Image.asset('images/dice1.png'),),
+          ),
+          Expanded(child: TextButton(onPressed:(){ },child: Image.asset('images/dice1.png'),),
+          ),
+        ],
+      ),
+    );
+  }
+}
